0a660de2777678513984ddb7c9d1ad8cee6eaace
Mo <malhayek@bruinmail.slcc.edu>
2018-10-15 12:51:23 -0600

    dev testin

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-
-# n is the number in question    
+# n is the number in question
 # STEP # 1  Is n composite?
 # STEP # 2 select an integer b at random with 1 <b<n
 # STEP # 3 whether n passes Miller’s test to the base b
@@ -9,11 +8,12 @@
 
 
 # STEP # 6 with 10 iterations, the probability that
-#the algorithm decides that n is prime when it really is composite is less than 1 in 1,000,000.
+# the algorithm decides that n is prime when it really is composite is less than 1 in 1,000,000.
 
 import random
 
-def rabinMiller(n , k):
+
+def rabinMiller(n, k):
     # Returns True if num is a prime number.
     # STEP 1 b a random 1<b<n-1
     # b = random.randint(1,n)
@@ -24,31 +24,39 @@ def rabinMiller(n , k):
         t += 1
     # STEP # 3 whether n passes Miller’s test to the base b
     # STEp # 4 we perform the miller test k times, where k is a positive integer.
-    for trials in range(k): # try to falsify num's primality 5 times
+    for trials in range(k):  # try to falsify num's primality 5 times
         b = random.randrange(2, n - 1)
+        failureCounter = 0
         remainder = pow(b, s, n)
-        #remainder = pow(b,e,n)
-        if remainder != 1: # this test does not apply if v is 1.
-            i = 0
+        # remainder = pow(b,e,n)
+        if remainder != 1:  # this test does not apply if v is 1.
+            i = -1
             while remainder != (n - 1):
+                #print(i)
+                #print(t)
                 if i == t - 1:
                     return False
                 else:
                     i = i + 1
                     remainder = (remainder ** 2) % n
     return True
-    
-def isPrime(num , k):
+
+
+def isPrime(num, k):
     # edge cases test since 2 and 3 is prime
     # 1 is not a prime num
     if (num < 2):
-        return False 
+        return False
     if (num == 2 or num == 3):
         return True
-    if (k <= 3 ):
+    if (k <= 3):
         print("for better result please set k to 3 < k < 10")
-    return rabinMiller(num , k)
+    return rabinMiller(num, k)
+
 
 n = input('Enter your number in question:')
 k = input('Enter your k (3-10) :')
-
+number = int(n)
+loop = int(k)
+result = isPrime(number,loop)
+print(result)
